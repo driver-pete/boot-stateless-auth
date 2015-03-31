b55a53349fc838198d1f94a76d1481ad0aeca5c9
olegsinyavskiy <olegsinyavskiy@gmail.com>
2015-03-31 14:08:44 -0700

    trying to implement in memory user details

--- src/main/java/com/jdriven/stateless/security/UserDetailsService.java
+++ src/main/java/com/jdriven/stateless/security/UserDetailsService.java
@@ -1,7 +1,12 @@
 package com.jdriven.stateless.security;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AccountStatusUserDetailsChecker;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
@@ -11,15 +16,47 @@ public class UserDetailsService implements org.springframework.security.core.use
 	@Autowired
 	private UserRepository userRepo;
 
-	private final AccountStatusUserDetailsChecker detailsChecker = new AccountStatusUserDetailsChecker();
-
 	@Override
 	public final User loadUserByUsername(String username) throws UsernameNotFoundException {
 		final User user = userRepo.findByUsername(username);
 		if (user == null) {
 			throw new UsernameNotFoundException("user not found");
 		}
-		detailsChecker.check(user);
 		return user;
 	}
 }
+
+
+//@Service
+//public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+//
+////	@Autowired
+////	private UserRepository userRepo;
+////
+////	private final AccountStatusUserDetailsChecker detailsChecker = new AccountStatusUserDetailsChecker();
+//
+//	@Override
+//	public final User loadUserByUsername(String username) throws UsernameNotFoundException {
+////		final List<SimpleGrantedAuthority> authorities = new ArrayList<SimpleGrantedAuthority>() {
+////			private static final long serialVersionUID = 1L;
+////
+////		{
+////            this.add(new SimpleGrantedAuthority());
+////        }};
+////        
+//        User user = new User(username);
+//        
+//        user.grantRole(UserRole.valueOf("ROLE_ADMIN"));
+//        //UserRole.valueOf(authority)
+//        
+//        return new User(username);
+//        
+////		final User user = userRepo.findByUsername(username);
+////		if (user == null) {
+////			throw new UsernameNotFoundException("user not found");
+////		}
+//		//detailsChecker.check(user);
+////		return user;
+//	}
+//}
+
